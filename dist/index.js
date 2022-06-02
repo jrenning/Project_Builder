3fad68861a236c66ccada1564b07564509001809
jrenning <jackalexrenning@gmail.com>
2022-06-01 21:50:24 -0500

    Cleaned up the rest of the javascript functions for the project submissions and file showing

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,9 @@ const open_dir = window.__TAURI__.dialog.open
 // languages supported 
 const languages = ["python", "javascript"]
 
+
+
 /* Basic Settings*/
-
-
 
 
 function initializeSettings() {
@@ -46,9 +46,9 @@ async function displayPathSettings(language) {
     document.getElementById(selector).innerHTML = 'Current Path:' + text
 }
 
-
+// update settings based on language selected
 let updateSettings = function(language) {
-    return async function curried_func(e) {
+    return async function asyncSettings(e) {
         e.preventDefault()
         path = 'C:/'
         let result = await open_dir({defaultPath: path, directory: true, multiple: false}).catch(function(err) {
@@ -141,200 +141,146 @@ PageTransitions()
 
 /* Form Submissions*/
 
+// global path variable that is used instead of the predefined project paths
+let new_path = ''
+let last_path_used = ''
 
+function initializeProjectForm() {
+    for(let i=0; i<languages.length; i++) {
+        let submit_form = document.getElementById('submit-' + languages[i])
+        submit_form.addEventListener("submit", ProjectSubmission(languages[i]))
+    }
+    for(let i=0; i<languages.length; i++) {
+        let folder_show = document.getElementById('folder-existing-' + languages[i])
+        folder_show.addEventListener("click", showFiles(languages[i]))
+    }
+
+}
 
 
 // selector to help show the chosen folder path for 
 // existing folder selection, used to check if path
 // needs to be changed as well
-const chosen_folder_py = document.getElementById('chosen-folder-py')
-// global path variable that is used instead of the predefined project paths
-let new_path = ''
-let last_path_used = ''
 
+let ProjectSubmission  = function(language) {
+    return async function asyncSubmission(e) {
+        e.preventDefault()
+        // get form data 
+        const formData = new FormData(e.target)
+        const formProps = Object.fromEntries(formData)
+        // get default path from settings
+        let path = await readSettings("path", language, "path")
 
-const py_submit = document.getElementById('pysubmit')
-py_submit.addEventListener("submit", pythonSubmission)
+        let selector = "chosen-folder-" + language
+        const chosen_folder = document.getElementById(selector)
 
-
-async function pythonSubmission (e) {
-    // prevent page refresh
-    e.preventDefault()
-    // get form data 
-    const formData = new FormData(e.target)
-    const formProps = Object.fromEntries(formData)
-    // get default path from settings
-    let path = await readSettings("path", "python", "path")
-
-    // checks if other folder was chosen
-    if (chosen_folder_py.innerHTML) {
-        // makes sure not to set path as a null folder 
-        if (chosen_folder_py.innerHTML !== 'Chosen folder: null\\') {
-            path = new_path
+        if (chosen_folder.innerHTML) {
+            // makes sure not to set path as a null folder 
+            if (chosen_folder.innerHTML !== 'Chosen folder: null\\') {
+                path = new_path
+            }
         }
-    }
+        console.log(`Path after change? ${path}`)
+        // make a directory with the project name 
+        invoke('make_dir', {dir: formProps['project-name'], path: path})
+        // create a file in the new directory  
+        let project_path = path + formProps['project-name'] + "\\"
 
-    // make a directory with the project name 
-    invoke('make_dir', {dir: formProps['project-name'], path: path})
-    // crate a main.py file in the new directory  
-    let project_path = path + formProps['project-name'] + "\\"
+        let file_create_list = []
 
-    const file_creation = await invoke('write_file', {fileName: 'main.py', dir: project_path}).then().catch(function(err) {
-        console.log(err)
-    })
-    // runs if file already exists 
-    if (!file_creation) {
-        alert('Project already exists')
-        new_path = ''
-        return
-    }
-
-    // make nev if selected 
-    if (formProps['env-setup'] == 'on') {
-        console.log('Trying to make env...')
-        const env_command  = await new Command("make_env", project_path, {cwd: project_path}).execute().catch(function(err) {
-            console.log(err)
-        }) 
-    }
-
-    // do github actions if they are turned on 
-    await githubActions(formProps['git-setup'], formProps['github-link'], project_path)
-    // ending message abd cleanup
-    endingMessage(formProps['project-name'], project_path)
-
-}
-
-// Javascript submissions 
-
-const js_submit = document.getElementById('jssubmit')
-js_submit.addEventListener("submit", javascriptSubmission)
-
-const chosen_folder_js = document.getElementById('chosen-folder-js')
-
-
-async function javascriptSubmission(e) {
-    // prevent page refresh
-    e.preventDefault()
-    // get form data 
-    const formData = new FormData(e.target)
-    const formProps = Object.fromEntries(formData)
-    let path = await readSettings("path", "javascript", "path")
-
-    // checks if other folder was chosen
-    if (chosen_folder_js.innerHTML) {
-        // makes sure not to set path as a null folder 
-        if (chosen_folder_py.innerHTML !== 'Chosen folder: null\\') {
-            path = new_path
+        if (language == "python") {
+            file_create_list.push("main.py")
         }
-    }
+        if (language == "javascript") {
+            file_create_list.push("index.js", "index.css", "index.html", "index.scss")
+        }
+        for (let i=0; i<file_create_list.length; i++) {
+            let file_creation = await invoke('write_file', {fileName: file_create_list[i], dir: project_path}).then().catch(function(err) {
+                console.log(err)
+            })
+            // on first pass check for duplicate file, break 
+            if (i == 0) {
+                // runs if file already exists 
+                if (!file_creation) {
+                    alert('Project already exists')
+                    new_path = ''
+                    return
+                }
+            }
 
-    // make a directory with the project name 
-    invoke('make_dir', {dir: formProps['project-name'], path: path})
-    // crate a main.py file in the new directory  
-    let project_path = path + formProps['project-name'] + "\\"
-
-    const file_creation = await invoke('write_file', {fileName: 'main.py', dir: project_path}).then().catch(function(err) {
-        console.log(err)
-    })
-    // runs if file already exists 
-    if (!file_creation) {
-        alert('Project already exists')
-        new_path = ''
-        return
-    }
-    invoke('write_file', {fileName: 'index.js', dir: project_path})
-    invoke('write_file', {fileName: 'styles.css', dir: project_path})
-    invoke('write_file', {fileName: 'styles.scss', dir: project_path})
-
-    // do github actions if they are turned on 
-    await githubActions(formProps['git-setup'], formProps['github-link'], project_path)
-    endingMessage(formProps['project-name'], project_path)
-
-}
-
-
-// Helper functions for the different submissions 
-
-async function githubActions(git_setup, github_link, project_path) {
-    if (git_setup == 'on') {
+        }
+        // if python make env (if selected as an option)
+            // make nev if selected 
+        if (formProps['env-setup'] == 'on') {
+            console.log('Trying to make env...')
+            const env_command  = await new Command("make_env", project_path, {cwd: project_path}).execute().catch(function(err) {
+                console.log(err)
+            }) 
+        }
+        if (formProps['git-setup'] == 'on') {
         console.log("Trying to set up git...")
         const git_command = await new Command("git", project_path, {cwd: project_path}).execute().catch(function(err) {
             console.log(err)
         })
         invoke('write_file', {fileName: '.gitignore', dir: project_path})
+        }
+        if (formProps['github-link']) {
+            console.log('Trying to connect to github...')
+            const git_add_command = await new Command("git-add-all", ['add', '.'], {cwd: project_path}).execute().catch(function(err) {
+                console.log(err)
+            })
+            console.log('Added files to git...')
+
+            const git_commit_command = await new Command("git-commit", ['commit', '-m', 'Initial commit'], {cwd: project_path}).execute().catch(function(err) {
+                console.log(err)
+            })
+            console.log("Committed files to git...")
+
+            const git_branch_command = await new Command("github-branch-rename", ['branch', '-M', 'main'], {cwd: project_path}).execute().catch(function(err) {
+                console.log(err)
+            })
+            console.log("Renamed master branch to main...")
+
+            const git_connect_command = await new Command("github-link", ['remote', 'add', 'origin', github_link], {cwd: project_path}).execute().catch(function(err) {
+                console.log(err)
+            })
+            console.log("Connected repository to github...")
+            const git_push_initial = await new Command("github-push-initial", ['push', '-u', 'origin', 'main'], {cwd: project_path}).execute().catch(function(err) {
+                console.log(err)
+            })
+            console.log("Pushed initial commit to github...")
+        }
+        // set global last path used to path used 
+        last_path_used = project_path
+
+        // set new_path back to default 
+        new_path = ''
+
+        //alert that project was made 
+        alert(`Project ${formProps['project-name']} was successfully created at the path ${project_path}`)
+
     }
-    if (github_link) {
-        console.log('Trying to connect to github...')
-        const git_add_command = await new Command("git-add-all", ['add', '.'], {cwd: project_path}).execute().catch(function(err) {
-            console.log(err)
-        })
-        console.log('Added files to git...')
+}
 
-        const git_commit_command = await new Command("git-commit", ['commit', '-m', 'Initial commit'], {cwd: project_path}).execute().catch(function(err) {
+let showFiles = function(language) {
+    return async function asyncFiles(e) {
+        e.preventDefault()
+        let path = await readSettings("path", language, "path")
+        let result = await open_dir({defaultPath: path, directory: true, multiple: false}).catch(function(err) {
             console.log(err)
         })
-        console.log("Committed files to git...")
+        new_path = result + '\\'
+        let selector = 'chosen-folder-' + language
+        let chosen_folder = document.getElementById(selector)
 
-        const git_branch_command = await new Command("github-branch-rename", ['branch', '-M', 'main'], {cwd: project_path}).execute().catch(function(err) {
-            console.log(err)
-        })
-        console.log("Renamed master branch to main...")
+        chosen_folder.innerHTML = `Chosen folder: ${new_path}`
 
-        const git_connect_command = await new Command("github-link", ['remote', 'add', 'origin', github_link], {cwd: project_path}).execute().catch(function(err) {
-            console.log(err)
-        })
-        console.log("Connected repository to github...")
-        const git_push_initial = await new Command("github-push-initial", ['push', '-u', 'origin', 'main'], {cwd: project_path}).execute().catch(function(err) {
-            console.log(err)
-        })
-        console.log("Pushed initial commit to github...")
+
     }
 }
 
-function endingMessage(project_name, project_path) {
-    // set global last path used to path used 
-    last_path_used = project_path
+initializeProjectForm()
 
-    // set new_path back to default 
-    new_path = ''
-
-    //alert that project was made 
-    alert(`Project ${project_name} was sucsessfully created at the path ${project_path}`)
-}
-
-// Handling popup folder selection 
-const file_show_py = document.getElementById('folder-existing-py')
-file_show_py.addEventListener("click",showFilesPy)
-
-const file_show_js = document.getElementById('folder-existing-js')
-file_show_js.addEventListener("click", showFilesJs)
-
-
-
-/* Showing file structure*/
-
-
-async function showFilesPy(e) {
-    e.preventDefault()
-
-    let path = 'C:\\Projects\\Python\\'
-    let result = await open_dir({defaultPath: path, directory: true, multiple: false}).catch(function(err) {
-        console.log(err)
-    })
-    new_path = result + '\\'
-    chosen_folder_py.innerHTML = `Chosen folder: ${new_path}`
-}
-
-async function showFilesJs(e) {
-    e.preventDefault()
-
-    let path = 'C:\\Projects\\Javascript\\'
-    let result = await open_dir({defaultPath: path, directory: true, multiple: false}).catch(function(err) {
-        console.log(err)
-    })
-    new_path = result + '\\'
-    chosen_folder_js.innerHTML = `Chosen folder: ${new_path}`
-}
 
 
 /* Other Components*/
